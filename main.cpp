cb9879bc2eadd1881da178a0e406f8f9c30e994e
Tomas Libal <tomas.libal@unrulymedia.com>
2015-11-19 22:29:45 +0000

    add empty netbeans project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: tomaslibal
+ *
+ * Created on 19 November 2015, 22:27
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
