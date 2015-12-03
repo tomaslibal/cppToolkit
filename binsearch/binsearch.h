03ba60bad4917540460935d55e54434d4c06ed04
Tomas Libal <tomas@libal.eu>
2015-12-03 19:40:35 +0000

    add binsearch.h file

---
+++ binsearch/binsearch.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   binsearch.h
+ * Author: tlibal
+ *
+ * Created on December 3, 2015, 7:37 PM
+ */
+
+#ifndef BINSEARCH_H
+#define	BINSEARCH_H
+
+template <typename T> T binsearch(T lookup, T* list, const int listLen);
+
+#endif	/* BINSEARCH_H */
+
