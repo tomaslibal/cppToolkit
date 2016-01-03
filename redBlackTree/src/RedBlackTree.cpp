4231902306127b0b56ad45fb7b9d304c4610cc90
Tomas Libal <tomas@libal.eu>
2016-01-03 19:33:02 +0000

    add red black tree class

---
+++ redBlackTree/src/RedBlackTree.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RedBlackTree.cpp
+ * Author: tlibal
+ * 
+ * Created on January 3, 2016, 7:31 PM
+ */
+
+#include "RedBlackTree.h"
+
+RedBlackTree::RedBlackTree() {
+}
+
+RedBlackTree::RedBlackTree(const RedBlackTree& orig) {
+}
+
+RedBlackTree::~RedBlackTree() {
+}
+
---
+++ redBlackTree/src/RedBlackTree.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   RedBlackTree.h
+ * Author: tlibal
+ *
+ * Created on January 3, 2016, 7:31 PM
+ */
+
+#ifndef REDBLACKTREE_H
+#define REDBLACKTREE_H
+
+class RedBlackTree {
+public:
+    RedBlackTree();
+    RedBlackTree(const RedBlackTree& orig);
+    virtual ~RedBlackTree();
+private:
+
+};
+
+#endif /* REDBLACKTREE_H */
+
