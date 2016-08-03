d10b307e4c3aab8d653d78c72eec4da86215a047
Tomas Libal <tomas.libal@unrulymedia.com>
2016-08-03 12:21:07 +0100

    add quicksort tests

---
+++ quicksort/test/quicksortTest.cpp
@@ -0,0 +1,49 @@
+/*
+ * File:   quicksortTest.cpp
+ * Author: tlibal
+ *
+ * Created on Dec 20, 2015, 3:50:26 PM
+ */
+
+#include "quicksortTest.h"
+#include <cstring>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+CPPUNIT_TEST_SUITE_REGISTRATION(quicksortTest);
+
+quicksortTest::quicksortTest() {
+}
+
+quicksortTest::~quicksortTest() {
+}
+
+void quicksortTest::printIntArray(int* array, int len) {
+    cout << "{";
+
+    for (int i = 0; i < len; i++) {
+        cout << array[i] << ", ";
+    }
+
+    cout << "}" << endl;
+}
+
+void quicksortTest::assertIntArrayEquals(int* arrayA, int* arrayB, int len)
+{
+    for (int i = 0; i < len; i++) {
+        if (arrayA[i] != arrayB[i]) {
+            CPPUNIT_FAIL("Assertion failed for IntArrayEquals");
+        }
+    }
+}
+
+void quicksortTest::testBasicCase() {
+    int array[5] = { 4, 1, 5, 8, 7 };
+    int expected[5] = { 1, 4, 5, 7, 8 };
+
+    quicksort(array, 0, 4);
+    
+    assertIntArrayEquals(array, expected, 5);
+}
---
+++ quicksort/test/quicksortTest.h
@@ -0,0 +1,35 @@
+/*
+ * File:   quicksortTest.h
+ * Author: tlibal
+ *
+ * Created on Dec 20, 2015, 3:50:25 PM
+ */
+
+#ifndef QUICKSORTTEST_H
+#define	QUICKSORTTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "../src/quicksort.h"
+
+class quicksortTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(quicksortTest);
+
+    CPPUNIT_TEST(testBasicCase);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    quicksortTest();
+    virtual ~quicksortTest();
+
+private: 
+    void testBasicCase();
+
+    void printIntArray(int* array, int len);
+
+    void assertIntArrayEquals(int* arrayA, int* arrayB, int len);
+};
+
+#endif	/* QUICKSORTTEST_H */
+
