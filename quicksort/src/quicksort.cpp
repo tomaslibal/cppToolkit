0127f6fb891e0f18324056b1b3c225f509915d24
Tomas Libal <tomas.libal@unrulymedia.com>
2016-08-03 12:20:52 +0100

    add implementation of quicksort

--- quicksort/src/quicksort.cpp
+++ quicksort/src/quicksort.cpp
@@ -1,6 +1,39 @@
 #include "quicksort.h"
 
-template <typename T> void quicksort(T** array, int lo, int hi)
+template <typename T> void quicksort(T* array, int lo, int hi)
 {
+    int pivot;
 
+    if (hi > lo) {
+        partition(array, lo, hi, pivot);
+        quicksort(array, lo, pivot - 1);
+        quicksort(array, pivot + 1, hi);
+    }
 }
+
+template <typename T> void partition(T* array, int lo, int hi, int& pivot)
+{
+    int i, j;
+    T item;
+    T tmp;
+
+    item = array[lo];
+    j = lo;
+
+    for (i = lo + 1; i <= hi; i++) {
+        if (array[i] < item) {
+            j++;
+            tmp = array[j];
+            array[j] = array[i];
+            array[i] = array[j];
+        }
+    }
+
+    pivot = j;
+    tmp = array[lo];
+    array[lo] = array[pivot];
+    array[pivot] = tmp;
+}
+
+template void quicksort(int* array, int lo, int hi);
+template void partition(int* array, int lo, int hi, int& pivot);
--- quicksort/src/quicksort.h
+++ quicksort/src/quicksort.h
@@ -1,6 +1,8 @@
 #ifndef QUICKSORT_H
-#def QUICKSORT_H
+#define QUICKSORT_H
 
-template <typename T> void quicksort(T** array, int lo, int hi);
+template <typename T> void quicksort(T* array, int lo, int hi);
+
+template <typename T> void partition(T* array, int lo, int hi, int& pivot);
 
 #endif /* QUICKSORT_H */
