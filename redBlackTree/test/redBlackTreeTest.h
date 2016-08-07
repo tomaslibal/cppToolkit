32ba2c7aacf8e544154bbee54ff359c8a334c420
Tomas Libal <tomas.libal@unrulymedia.com>
2016-08-07 14:01:06 +0100

    whitespace removed

--- redBlackTree/test/redBlackTreeTest.h
+++ redBlackTree/test/redBlackTreeTest.h
@@ -17,7 +17,7 @@ class redBlackTreeTest : public CPPUNIT_NS::TestFixture {
 
     CPPUNIT_TEST(testCreateVoid);
     CPPUNIT_TEST(testRootIsBlack);
-    CPPUNIT_TEST(testIsNilTest);
+    CPPUNIT_TEST(testIsNilTest);    
 
     CPPUNIT_TEST_SUITE_END();
 
