bba3ac26d3dc6b47be80cef08fa073bfb07d5b7f
Tomas Libal <tomas@libal.eu>
2016-12-28 17:28:37 +0800

    change int* to std::vector<int>
    - passing raw arrays around = passing pointers and having to memory management by hand, hoping that std::vector will be more convenient in the end

--- heap/src/Heap.h
+++ heap/src/Heap.h
@@ -1,19 +1,21 @@
 #ifndef HEAP_H 
 #define	HEAP_H
 
+#include <vector>
+
 class HeapArray
 {
 private:
-    int* array;
+    std::vector<int> oarray;
 public:
-    int len;
-    int heapsize;
-    static HeapArray fromArray(int* a);
+    HeapArray(std::vector<int> const& a) : oarray(a.begin(), a.end()) {}
+    int len = 0;
+    int heapsize = 0;
 };
 
-void max_heapify(HeapArray& ha, int index);
-void build_max_heap(HeapArray& ha);
+void max_heapify(HeapArray & ha, int index);
+void build_max_heap(HeapArray & ha);
 
-int heapsearch(int* array, int needle);
+int heapsearch(std::vector<int> const& array, int needle);
 
 #endif
