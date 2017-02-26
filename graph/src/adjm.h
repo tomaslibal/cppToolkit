23ae4c509844a0522e17d12ef32cea7a3218d0a7
Tomas Libal <tomas@libal.eu>
2017-02-26 19:21:23 +0800

    add adjacency matrix

---
+++ graph/src/adjm.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stdlib.h>
+
+namespace cpptoolkit {
+    /**
+     * adjacency matrix
+     * 
+     */
+    class adjm {
+    private:    
+    public:
+        int** matrix;
+        int m;
+        
+        adjm(int m) 
+        {
+            int initial_value = 0;
+            this->m = m;
+            
+            matrix = (int**) malloc(sizeof(int*) * m);
+            if (matrix == nullptr) {
+                // malloc error
+                exit(1);
+            }
+            
+            for (int i = 0; i < m; i++) {
+                
+                matrix[i] = (int*) malloc(sizeof(int) * m);
+                if (matrix[i] == nullptr) {
+                    // malloc error
+                    exit(1);
+                }
+                
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = initial_value;
+                }
+            }
+        }
+        
+        virtual ~adjm()
+        {
+            for (int i = 0; i < this->m; i++) {
+                free(matrix[i]);
+            }
+            free(matrix);
+        }
+            
+    }
+}
