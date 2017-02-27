eaf5bfe46a2b6dc094b04756b8ad6937a16e1689
Tomas Libal <tomas@libal.eu>
2017-02-27 12:27:00 +0800

    add pseudo code

---
+++ kruskal/src/kruskal.cpp
@@ -0,0 +1,19 @@
+#include "kruskal.h"
+
+
+std::vector<cpptoolkit::Edge> cpptoolkit::kruskal()
+{
+    std::vector<cpptoolkit::Edge> mst;
+
+    for (auto vert : v) {
+        // make set vert
+    }
+    // sort edges by weight ASC
+    for (auto edge : e) {
+        if (findSet(edge.a) == findSet(edge.b)) {
+            // mst.add(edge)
+            // union a,b
+        }
+    }
+    return mst
+}
