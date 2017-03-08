f3becf3db60505eb6326369b682fb48bc8aaf813
Tomas Libal <tomaslibal@users.noreply.github.com>
2017-03-08 16:12:38 +0800

    Update haversine.h

--- haversine/src/haversine.h
+++ haversine/src/haversine.h
@@ -2,6 +2,7 @@
 
 /*
  * calculates distance between two lon,lat coordinates on a globe object of the radius r.
+ * Haversine: http://mathworld.wolfram.com/Haversine.html
  */
 namespace cpptoolkit {
     double haversine(double lon1, double lat1, double lon2, double lat2, double r);
