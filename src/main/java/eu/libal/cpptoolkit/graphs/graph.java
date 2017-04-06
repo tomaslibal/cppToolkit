9d07ad401eb86216c144bbdc2a562005f918d466
Tomas Libal <tomas@libal.eu>
2017-04-06 23:28:32 +0800

    add stub of the java Graph class

---
+++ src/main/java/eu/libal/cpptoolkit/graphs/graph.java
@@ -0,0 +1,46 @@
+package eu.libal.cpptoolkit.graphs;
+
+public class Graph {
+    public Graph() {
+    }
+
+    public void <T> addVertex(Vertex<T> v) {
+
+    }
+
+    public boolean <T, U> addEdge(Edge<Vertex<T>, Vertex<U>> e) {
+
+    }
+
+    public boolean removeVertex(Vertex<T> v) {
+
+    }
+
+    public boolean <T, U> removeEdge(Edge<Vertex<T>, Vertex<U>> e) {
+
+    }
+
+    public boolean <T, U> adjacent(Vertex<T> v, Vertex<U> w) {
+
+    }
+
+    public List<Vertex> <T> neighbors(Vertex<T> v) {
+
+    }
+
+    public T <T> getVertexValue(Vertex<T> v) {
+
+    }
+
+    public void <T> setVertexValue(Vertex<T> v, T val) {
+
+    }
+
+    public Double <T, U> getEdgeValue(Edge<Vertex<T>, Vertex<U>> e) {
+    
+    }
+
+    public void <T, U> setEdgeValue(Edge<Vertex<T>, Vertex<U>> e, Double val) {
+
+    }
+}
