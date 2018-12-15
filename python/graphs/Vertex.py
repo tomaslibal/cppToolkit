909fd8c428508ff25511308e604080c15cb6c85f
Tomas Libal <tomas@libal.eu>
2018-12-15 14:39:19 +0800

    add a binary vertex

--- python/graphs/Vertex.py
+++ python/graphs/Vertex.py
@@ -13,3 +13,19 @@ class Vertex:
 
     def __repr__(self):
         return "<Vertex(value={})>".format(self.value)
+
+
+class BinaryVertex:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __eq__(self, other):
+        return isinstance(other, BinaryVertex) and other.value == self.value
+
+    def __ne__(self, other):
+        return not (other == self)
+
+    def __repr__(self):
+        return "<BinaryVertex(value={})>".format(self.value)
