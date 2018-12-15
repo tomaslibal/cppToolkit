0ee7f9083288d815e09f151f9ef86c0ce225ccae
Tomas Libal <tomas@libal.eu>
2018-12-15 22:39:26 +0800

    use list instead of defaultdict

--- python/graphs/Graph.py
+++ python/graphs/Graph.py
@@ -1,8 +1,31 @@
-from collections import defaultdict
-
 class Graph:
     def __init__(self):
-        self.graph = defaultsdict(list)
+        self.vertices = []
+        self.edges = []
 
     def add_edge(self, u, v):
-        self.graph[u].append(v)
+        self.edges.append((u, v))
+
+    def add_vertex(self, v):
+        self.vertices.append(v)
+
+    def remove_vertex(self, v):
+        try:
+            idx = self.vertices.index(v)
+            del self.vertices[idx]
+            return True
+        except ValueError:
+            return False
+
+    def remove_edge(self, u, v):
+        try:
+            idx = self.edges.index((u, v))
+            del self.edges[idx]
+            return True
+        except ValueError:
+            return False
+
+    def adjacent(self, u, v):
+        return (u, v) in self.edges
+
+
