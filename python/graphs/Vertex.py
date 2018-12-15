2e20c5199bfa58d7f3c68d326646f6082f40c091
Tomas Libal <tomas@libal.eu>
2018-12-15 12:43:56 +0800

    add visited attribute

--- python/graphs/Vertex.py
+++ python/graphs/Vertex.py
@@ -3,9 +3,13 @@ class Vertex:
         self.value = value
         self.adjacent = []
         self.parent = None
+        self.visited = False
 
     def __eq__(self, other):
         return isinstance(other, Vertex) and other.value == self.value
+
+    def __ne__(self, other):
+        return not (other == self)
 
     def __repr__(self):
         return "<Vertex(value={})>".format(self.value)
